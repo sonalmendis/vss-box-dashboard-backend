7d73efb258d169e347decdbd6e373c8e1a0eeaa2
sonal <sonal.cmendis@gmail.com>
2023-06-06 22:28:12 +0530

    Setup server variables

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,5 @@
 module.exports = ({ env }) => ({
-  "vercel-deploy": {
+  "netlify-deployments": {
     enabled: true,
-    config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_Khzeb6cyoKK5nhgYqJRJ2saCEJcO/E4pDf6HOLq",
-      apiToken: "NhSArhHUxL9XnSqzzyxUW3HZ",
-      appFilter: "vss-box-company-dashboard-backend",
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    },
   },
 });
