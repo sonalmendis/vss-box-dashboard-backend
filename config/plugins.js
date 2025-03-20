b68299178529c1ca48f7e4bd1f3979c66ba15a7e
sonalcm <sonal.cmendis@gmail.com>
2025-03-20 10:28:56 +0530

    bucket adjustment

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,16 @@
 module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    config: {
-      provider: "strapi-provider-upload-do",
-      providerOptions: {
-        key: "DO00K4BV7R63YEPVUE94",
-        secret: "/V+s8cW9B2OZEpmpLezZa2SBGCEu0V1Ap8v8J0Khjh8",
-        endpoint: "sgp1.digitaloceanspaces.com",
-        space: "strapi-images",
-      },
+    // ...
+    upload: {
+        config: {
+            provider: "strapi-provider-upload-do",
+            providerOptions: {
+                key: env("DO_SPACE_ACCESS_KEY"),
+                secret: env("DO_SPACE_SECRET_KEY"),
+                endpoint: env("DO_SPACE_ENDPOINT"),
+                space: env("DO_SPACE_BUCKET"),
+            },
+        },
     },
-  },
 
-  // ...
+    // ...
 });
