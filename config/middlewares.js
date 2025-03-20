a7f6b227d8bb955e1085b401cac2c91490247156
sonalcm <sonal.cmendis@gmail.com>
2025-03-20 11:01:24 +0530

    middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,25 +1,36 @@
-module.exports = [
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", "*.digitaloceanspaces.com"],
-          "media-src": ["'self'", "data:", "blob:"],
-          upgradeInsecureRequests: null,
+export default [
+    "strapi::logger",
+    "strapi::errors",
+    // 'strapi::security',
+    {
+        name: "strapi::security",
+        config: {
+            contentSecurityPolicy: {
+                useDefaults: true,
+                directives: {
+                    "connect-src": ["'self'", "https:"],
+                    "img-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "fi4onf3.blr1.digitaloceanspaces.com",
+                    ],
+                    "media-src": [
+                        "'self'",
+                        "data:",
+                        "blob:",
+                        "fi4onf3.blr1.digitaloceanspaces.com",
+                    ],
+                    upgradeInsecureRequests: null,
+                },
+            },
         },
-      },
     },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+    "strapi::cors",
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
